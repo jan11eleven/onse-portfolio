aef9ef8f709bd04a9fba51b946e51060b03bfdc1
Jan Eleven Salvador <2.janelevensalvador@gmail.com>
2024-11-18 23:34:57 +0800

    explicitly put tailwind responsive sizes

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -32,6 +32,14 @@ const config: Config = {
 					},
 				},
 			},
+			theme: {
+				screens: {
+					sm: "640px",
+					md: "768px",
+					lg: "1024px",
+					xl: "1280px",
+				},
+			},
 		},
 	},
 	plugins: [],
