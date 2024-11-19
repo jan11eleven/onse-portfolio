314562938936edd0b87d07e96aae4f58df6d317a
Jan Eleven Salvador <2.janelevensalvador@gmail.com>
2024-11-19 13:35:33 +0800

    add responsive

--- app/components/Landing.tsx
+++ app/components/Landing.tsx
@@ -6,7 +6,7 @@ import { FaFacebook, FaLinkedin, FaViber } from "react-icons/fa";
 export default function Landing() {
 	return (
 		<div className="text-xl sm:text-2xl lg:text-4xl min-h-[calc(100vh-84px)] lg:flex lg:flex-row lg:justify-between lg:justify-items-center flex-col lg:pt-40">
-			<div className=" lg:pl-40 pt-20 text-center md:text-left flex-col justify-end sm:justify-items-center lg:justify-items-start flex-1">
+			<div className=" lg:pl-40 pt-20 text-center md:text-left flex-col justify-end justify-items-center lg:justify-items-start flex-1">
 				<p className="figtree-font mb-2 lg:mb-6 select-none">
 					I AM{" "}
 					<span className="alegreya-font text-3xl sm:text-4xl lg:text-6xl text-bold text-primary">
@@ -16,7 +16,7 @@ export default function Landing() {
 				<p className="figtree-font mb-8 lg:mb-14 select-none">
 					a Java & Appian Developer
 				</p>
-				<div className="w-2/4 lg:w-4/4 mb-14 lg:ml-20 lg:hidden">
+				<div className="image-1 w-2/4 lg:w-4/4 mb-14 lg:ml-20 lg:hidden">
 					<Image
 						src="/profile.png"
 						alt="Description of image"
